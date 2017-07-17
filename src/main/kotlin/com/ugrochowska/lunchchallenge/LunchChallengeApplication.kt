a63cef5674eb33ed2050f0a28d5587f0c9342f77
Urszula Grochowska <urszula.grochowska@allegrogroup.com>
2017-07-17 11:13:39 +0200

    initial commit

---
+++ src/main/kotlin/com/ugrochowska/lunchchallenge/LunchChallengeApplication.kt
@@ -0,0 +1,11 @@
+package com.ugrochowska.lunchchallenge
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class LunchChallengeApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(LunchChallengeApplication::class.java, *args)
+}
